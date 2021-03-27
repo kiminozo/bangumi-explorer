d284d27cc3f91d32dc61c3cf5df0d7e99a96eb50
Kiminozo <kiminozo.eien@gmail.com>
2021-03-27 18:12:49 +0800

    搜索页面

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,47 +2,87 @@ import React, { useState } from 'react'
 import Head from 'next/head'
 import Link from 'next/link'
 import Nav from '../components/nav'
-import { Grid, Input, Button } from 'semantic-ui-react';
+import { Grid, Input, Item, Container, Icon } from 'semantic-ui-react';
 import _ from 'lodash'
 
 import 'semantic-ui-css/semantic.min.css'
-import { IndexStore, StoreItem } from '../store/store';
+import { StoreItem } from '../store/store';
 
-interface State {
+interface Result {
   items: StoreItem[]
 }
 
 //const store = new IndexStore();
+const url = "http://localhost:3000/anime/";
 
-export async function getServerSideProps() {
-  // Fetch data from external API
-  const res = await fetch(`https://.../data`)
-  const data = await res.json()
+// export async function getServerSideProps() {
+//   // Fetch data from external API
+//   const res = await fetch(`https://.../data`)
+//   const data = await res.json()
 
-  // Pass data to the page via props
-  return { props: { data } }
-}
+//   // Pass data to the page via props
+//   return { props: { data } }
+// }
 
 async function search(key: string, setFun: (items: StoreItem[]) => void) {
   //const items = await store.Search(key);
-  setFun([]);
+  if (!key || key === '') {
+    setFun([]);
+    return;
+  }
+  const response = await fetch(url + key);
+  const data: Result = await response.json();
+  //console.log("res:" + JSON.stringify(data));
+  const items = data.items;
+  if (items) {
+    setFun(items);
+  }
+}
+async function test(count: number, setFun: (items: number) => void) {
+  setFun(count + 1);
+}
+
+const ItemsGroup = (props: { items: StoreItem[] }) => {
+  const { items } = props;
+  return (<Item.Group divided>
+    {
+      items.map(item => (
+        <Item>
+          <Item.Image size='tiny' src={item.images.medium} />
+          <Item.Content>
+            <Item.Header>{item.name_cn}</Item.Header>
+            <Item.Meta>
+              <span >{item.name}</span>
+              <span >{item.air_date}</span>
+            </Item.Meta>
+            <Item.Description>{item.summary}</Item.Description>
+            <Item.Extra>
+              <Icon name='file' /> {item.path}
+            </Item.Extra>
+          </Item.Content>
+
+        </Item>
+      ))
+    }
+  </Item.Group>)
 }
 
 const Home = () => {
   const [items, setItems] = useState<StoreItem[]>([]);
+  const [count, setCount] = useState(0);
   return (
-    <>
-      <Input action='Search' placeholder='Search...' onChange={(data) => search(data.target.value, setItems)} />
+    <Container>
       <Grid>
-        {
-          _.forEach(items, (item) => (
-            <Grid.Column key={item.id}>
-              {item.name_cn}
-            </Grid.Column>
-          ))
-        }
-      </Grid>
-    </>
+        <Grid.Row>
+          <Input action='Search' placeholder='Search...'
+            onChange={(data) => search(data.target.value, setItems)} />
+          <br />
+        </Grid.Row>
+        <Grid.Row>
+          <ItemsGroup items={items} />
+        </Grid.Row>
+      </Grid >
+    </Container>
   )
 }
 
--- service/service.ts
+++ service/service.ts
@@ -1,12 +1,31 @@
 import express from "express";
+import next from 'next'
 import { IndexStore } from "../store/store";
-const app = express();
+
+//const app = express();
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler();
 const index = new IndexStore();
-app.get('/anime/:key', async (req, res) => {
-    const items = await index.Search(req.params.key);
-    console.log(req.params);
-    res.send(items);
-})
 
-const server = app.listen(8081);
+const server = express();
 
+async function run() {
+    await app.prepare();
+    server.get('/anime/:key', async (req, res) => {
+        const key = req.params.key;
+        if (!key) {
+            res.send({ items: [] });
+            return;
+        }
+        const items = await index.Search(req.params.key);
+        console.log("Search:" + req.params.key);
+        res.send({ items: items });
+    })
+    server.get('*', (req, res) => {
+        return handle(req, res)
+    })
+    server.listen(3000);
+}
+
+run();
