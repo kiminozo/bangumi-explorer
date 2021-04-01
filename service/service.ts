528e47c919e1f5de233686285adee776efeedc29
kiminozo <kiminozoeien@gmail.com>
2021-04-01 11:50:50 +0800

    build

--- service/service.ts
+++ service/service.ts
@@ -7,25 +7,31 @@ const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler();
 const index = new IndexStore();
+const port = process.env.PORT || 3000;
 
 const server = express();
 
 async function run() {
-    await app.prepare();
-    server.get('/anime/:key', async (req, res) => {
-        const key = req.params.key;
-        if (!key) {
-            res.send({ items: [] });
-            return;
-        }
-        const items = await index.Search(req.params.key);
-        //console.log("Search:" + req.params.key);
-        res.send({ items: items });
-    })
-    server.get('*', (req, res) => {
-        return handle(req, res)
-    })
-    server.listen(3000);
+    try {
+        await app.prepare();
+        server.get('/anime/:key', async (req, res) => {
+            const key = req.params.key;
+            if (!key) {
+                res.send({ items: [] });
+                return;
+            }
+            const items = await index.Search(req.params.key);
+            //console.log("Search:" + req.params.key);
+            res.send({ items: items });
+        })
+        server.get('*', (req, res) => {
+            return handle(req, res)
+        })
+        server.listen(port);
+    } catch (e) {
+        console.error(e);
+        process.exit(1);
+    }
 }
 
 run();
